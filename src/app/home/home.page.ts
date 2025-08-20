2b7b6499f29fb8cf3005d2decf7013c064b8fe1c
aluno <etecae023@hotmail.com>
2025-08-20 20:28:32 -0300

    Primeiro Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { retry, catchError } from 'rxjs';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +13,64 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+
+  CEP: string = '';
+  endereco:any = null;
+
+  UF: string = '';
+  cidade: string = '';
+  logradouro: string = '';
+  resultados: any[] = [];
+
+  opcao = 1;
+
+  // private apiURL ='https://viacep.com.br/ws'
+
+
+
+  constructor(private router: Router, private http: HttpClient, public toastController: ToastController) {}
+
+
+  buscarCep(){
+if(this.CEP && this.CEP.length === 8){
+ const URL = `https://viacep.com.br/ws/${this.CEP}/json/`
+
+ this.http.get<any>(URL).subscribe({next: (data) => {
+this.endereco = data.erro ?
+null : data;
+ },
+error :() => {
+  this.endereco= null
+}});
+} else{
+this.endereco = null;
+}
+
+
+}
+  buscarEndereco(){
+    if (this.UF && this.cidade && this.logradouro) {
+      const URL = `https://viacep.com.br/ws/${this.UF}/${this.cidade}/${this.logradouro}/json/`
+
+      this.http.get<any>(URL).subscribe({next: (data) => {
+        this.resultados = data.erro ?
+        null : data;
+        },
+        error :() => {
+          this.resultados= [];
+        }});
+      } else{
+      this.resultados = [];
+    }
+  }
+
+  escolha() {
+    this.opcao = this.opcao + 1
+    if (this.opcao % 2 == 0) {
+      console.log('busca por endereco')
+    } else {
+      console.log('busca por cep')
+    }
+  }
 
 }
